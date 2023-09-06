b538d3dc334f3b7659551b85e2a97c29e3662da0
JosephTong <joseph.tong@titansworld.co>
2023-09-06 16:28:35 +0800

    camera move

--- Assets/Map/Script/MapFreeCameraController.cs
+++ Assets/Map/Script/MapFreeCameraController.cs
@@ -5,23 +5,52 @@ using UnityEngine;
 public class MapFreeCameraController : MonoBehaviour
 {
     [SerializeField] private Transform m_CameraParent;
+    [SerializeField][Range(10f,100f)] private float m_Sensitivity = 10f;
     private Vector2 m_AimDragMouseStartPos = Vector2.zero;
     private Vector2 m_AimDragMouseEndPos = Vector2.zero;
-    private Vector3 m_MousePreviousPos = Vector3.zero;
+    private Vector2 m_MousePreviousPos = Vector2.zero;
     private bool m_IsCameraMoving = false;
     private Vector3 m_CameraDragStartPos;
 
-    public void OnDragMapBtnDown(){
-        m_IsCameraMoving = true;
-        m_MousePreviousPos = Input.mousePosition;
-        m_AimDragMouseStartPos = Input.mousePosition;
+
+    private void Start()
+    {
         m_CameraDragStartPos = m_CameraParent.position;
     }
 
-    public void OnDragMapBtnUp(){
+    private void Update()
+    {
+        if (m_IsCameraMoving)
+        {
+            // move free camera
+            MoveFreeCamera();
+        }
+    }
+
+    public void OnDragMapBtnDown()
+    {
+        m_MousePreviousPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        m_AimDragMouseStartPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        m_CameraDragStartPos = m_CameraParent.position;
+        m_IsCameraMoving = true;
+    }
+
+    public void OnDragMapBtnUp()
+    {
         m_IsCameraMoving = false;
         m_AimDragMouseStartPos = Vector2.zero;
-        m_MousePreviousPos = Vector3.zero;
+        m_MousePreviousPos = Vector2.zero;
         m_CameraDragStartPos = m_CameraParent.position;
+    }
+
+    private void MoveFreeCamera()
+    {
+        Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        float screenSlopeLength = Mathf.Sqrt(Screen.width * Screen.width + Screen.height * Screen.height );
+        float mouseToStartDistance = Vector2.Distance(mousePos, m_MousePreviousPos) / screenSlopeLength * m_Sensitivity ;
+        Vector2 direction = (m_MousePreviousPos - mousePos ).normalized;
+        Vector3 directionVThree = new Vector3(direction.x,0,direction.y);
+        m_CameraParent.position = m_CameraDragStartPos + directionVThree * mouseToStartDistance;
     }
 }
